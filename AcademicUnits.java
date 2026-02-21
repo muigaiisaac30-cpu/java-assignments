916b3dd43c9e4831c43e381260f660efddfc4dac
ISAAC JULIUS WAINAINA <muigaiisaac30@gmail.com>
2026-02-21 16:56:49 +0300

    Add files via upload

---
+++ AcademicUnits.java
@@ -0,0 +1,15 @@
+// NAME: ISAAC JULIUS WAINAINA
+// REGISTRATION NUMBER: CT100/G/22432/24public class AcademicUnits {
+
+public static void main(String[] args) {
+
+    System.out.println("ACADEMIC UNITS OFFERED THIS SEMESTER");
+    System.out.println("-------------------------------------");
+
+    System.out.println("1. Cloud Computing");
+    System.out.println("2. Mobile Computing");
+    System.out.println("3. Object Oriented Programming II");
+    System.out.println("4. Application Programming");
+    System.out.println("5. Network System Administration");
+    System.out.println("6. Object Oriented Analysis and Design");
+}
