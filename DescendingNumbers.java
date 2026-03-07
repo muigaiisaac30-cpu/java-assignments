f58346a54a4f7836ae8bb21706d50c01c5ce52de
ISAAC JULIUS WAINAINA <muigaiisaac30@gmail.com>
2026-03-07 10:04:21 +0300

    Add files via upload

---
+++ DescendingNumbers.java
@@ -0,0 +1,11 @@
+//REG NUMBER: CT100/G/22432/24
+//NAME: ISAAC JULIUS WAINAINA
+//WEEK 4 ASSIGNMENT
+public class DescendingNumbers {
+    public static void main(String[] args) {
+        // Loop from 100 down to 50
+        for (int i = 100; i >= 50; i--) {
+            System.out.println(i);
+        }
+    }
+}
